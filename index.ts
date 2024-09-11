f1b23825ac0e02455fca588e368157e1dc713cb5
Gabryella-Silva <gabryellaspereira7@gmail.com>
2024-09-10 22:29:15 -0300

    alterações

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ class Post{
     private _avatarUrl:string;
     private _imageUrl: string;
     private _isLiked: boolean = false;
-    private _descripiton:string;
+    private _descrition:string;
     private _CreatedAt:Date = new Date;
     private _numberOfLikes:number = 0
    
@@ -19,7 +19,7 @@ class Post{
         this._userName = userName.toLocaleUpperCase() // Permite acessar, chamar e modificar as propriedades do objeto,
         this._avatarUrl = avatarUrl;
          this._imageUrl = imageUrl;
-        this._descripiton = description;
+        this._descrition = description;
         
     }
 
@@ -37,9 +37,9 @@ class Post{
        const postContainer = document.getElementById(this._id)
         
         const btnLike =postContainer?.querySelector("#btn-like");
-    
+    /* 
         if (!btnLike) return;
-        btnLike.innerHTML = String(this._isLiked)
+        btnLike.innerHTML = String(this._isLiked) */
     }
 
     
@@ -90,13 +90,40 @@ class Post{
 
             </div>`
 
+        const postLikes = `  <div class="post-likes">
+
+            <i class="fa-solid fa-heart"></i>
+            <span> 100 likes </span>
+            </div>`
+
+        const postDescrition = `<div class="post-descrition"> 
+                                    <p> ${this._descrition} </p>
+                                </div>`
+            
+        
+
 
         postContainer.innerHTML = postHeader;
         postContainer.innerHTML += postImage;
         postContainer.innerHTML += postIcons;
+        postContainer.innerHTML += postLikes;
+        postContainer.innerHTML += postDescrition;
 
         const btnLike = postContainer.querySelector("#btn-like")
         btnLike?.addEventListener("click" ,() => this.like());
+
+        if (btnLike) {
+            btnLike.classList.remove('fa-regular');
+            btnLike.classList.add('fa-solid');
+            btnLike.classList.add('active');
+            
+    
+            //remove o coração preenchido e coloca o vazio
+        } else {
+            btnLike.classList.remove('fa-solid');
+            btnLike.classList.add('fa-regular'); 
+            btnLike.classList.remove('active');
+        }
 
       
         document.body.appendChild(postContainer);
@@ -115,9 +142,9 @@ for (let i = 0; i < 15; i++ ){
     const userName = faker.person.firstName();
     const avatarUrl = faker.image.avatar();
     const imageUrl = faker.image.urlLoremFlickr();
-    const description = faker.lorem.word();
+    const descrition = faker.lorem.paragraph();
 
-    const post = new Post(userName, avatarUrl, imageUrl, description);
+    const post = new Post(userName, avatarUrl, imageUrl, descrition);
 
     posts.push(post)
     post.toHtml()
@@ -128,6 +155,7 @@ for (let i = 0; i < 15; i++ ){
 
 posts[0].like();
 posts[0].like();
+
 console.log(posts[0])
 
 
